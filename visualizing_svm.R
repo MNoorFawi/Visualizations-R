c0ac76bf44789c6077fa895fad13e040bc024254
Muhammad N. Fawi <m.noor.fawi@gmail.com>
2018-09-20 01:59:33 +0200

    Update visualizing_svm.R

--- visualizing_svm.R
+++ visualizing_svm.R
@@ -9,7 +9,8 @@ x[y == 0, ] <- x[y == 0, ] ^ 2
 x[y == -1, ] <- x[y == -1, ] + 6
 data <- data.frame(x, Y = as.factor(y))
 ggplot(data, aes(x = X1, y = X2, col = Y)) +
-  geom_point()
+  geom_point() + theme_minimal() + 
+  scale_color_brewer(palette = "Set1")
 
 ## building our SVM MODEL
 svm.fit <- svm(Y ~ ., data = data, 
@@ -24,8 +25,10 @@ xgrid$ygrid <- factor(predict(svm.fit, xgrid))
 
 ## plotting the grid and store it
 g <- ggplot(xgrid, aes(x = X1, y = X2, col = ygrid)) +
-  geom_point(size = 2, alpha = 0.3)
+  geom_point(size = 2, alpha = 0.3) + theme_minimal() + 
+  scale_color_brewer(palette = "Set1")
 
 ## plotting the data on the grid to assess its performance
 g + geom_point(data = data, aes(x = data[, 1], y = data[, 2], col = Y)) +
-  guides(alpha = FALSE, size = FALSE)
+  guides(alpha = FALSE, size = FALSE) + theme_minimal() + 
+  scale_color_brewer(palette = "Set1")
