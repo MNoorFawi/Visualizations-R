77f4b50440574b811fc5a2b68dcec7263c009b21
Muhammad N. Fawi <m.noor.fawi@gmail.com>
2018-09-23 01:52:10 +0200

    Create ggbiplot_clusters.R

---
+++ ggbiplot_clusters.R
@@ -0,0 +1,18 @@
+protein <- read.table('protein.txt', sep = '\t', header = TRUE)
+vars <- colnames(protein)[-1]
+protein_mat <- scale(protein[, vars])
+# library(devtools)
+# install_github("vqv/ggbiplot")
+library(ggbiplot)
+protein_pca <- prcomp(protein_mat)
+cl <- factor(kmeans(protein_mat, 5)$cluster)
+ggbiplot(
+  protein_pca,
+  obs.scale = 1,
+  ellipse = TRUE,
+  circle = TRUE,
+  groups = cl
+) +
+  scale_color_brewer(palette = "Set1", name = "") +
+  theme_minimal()
+
